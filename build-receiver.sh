52b1083dc6da07106efd310781252dadfeecdda7
Navid Shakibapour <navid.shakibapour@gmail.com>
2020-09-28 09:04:28 -0400

    Removed build scripts

--- build-receiver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./set_tag
-docker run -v $(pwd)/receiver:/usr/src/receiver -w /usr/src/receiver maven:3.6.3-jdk-8 mvn clean package
-cp receiver/target/receiver.war liberty-receiver
-docker build -t navidsh/mq-sample-receiver:${TAG} ./liberty-receiver
-docker push navidsh/mq-sample-receiver:${TAG}
